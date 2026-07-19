cf934cd5637023faf6fbc6bba9a301140f3448cc
CodeLemmy <lemuelgaa25@gmail.com>
2026-07-19 16:30:48 +0800

    fixed 10, 14, and 28

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,11 +7,11 @@ public class PlayerMovement : MonoBehaviour
     private float horizontal;
     private float speed = 8f;
     private float jumpingPower = 16f;
-    private float isFacingRight = true;
+    private bool isFacingRight = true;
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform GroundCheck;
-    [SerailizeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask groundLayer;
 
     // Update is called once per frame
     void Update()
@@ -23,7 +23,7 @@ public class PlayerMovement : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
         }
 
-        if(Input.GetButtonUp("Jump") && rb.velocity.y, > 0f)
+        if(Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
         }
